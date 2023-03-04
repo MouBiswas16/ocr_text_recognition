c3dfc1b54d43c82b9c673495769ecae7382e1266
Mou Biswas <qtmeaowmou16@gmail.com>
2023-03-04 21:53:43 +0600

    main.dart & get_it injector

---
+++ lib/injection_container.dart
@@ -0,0 +1,34 @@
+import 'package:get_it/get_it.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'features/txt_ocr/data/datasource/txt_ocr_datasource.dart';
+import 'features/txt_ocr/data/datasource/txt_ocr_datasource_impl.dart';
+import 'features/txt_ocr/data/repository/txt_ocr_repository_impl.dart';
+import 'features/txt_ocr/domain/repository/txt_ocr_repository.dart';
+import 'features/txt_ocr/domain/usecases/txt_ocr_usecase.dart';
+import 'features/txt_ocr/presentation/bloc/txt_ocr_cubit.dart';
+
+final s1 = GetIt.instance;
+
+Future<void> init() async {
+  //cubit
+  s1.registerFactory(() => TxtOcrCubit(txtOcrUseCase: s1()));
+
+  //datasource
+  s1.registerLazySingleton<TxtOcrDataSource>(
+      () => TxtOcrDataSourceImpl(textRecognizer: s1()));
+
+  //repository
+  s1.registerLazySingleton<TxtOcrRepository>(
+      () => TxtOcrRepositoryImpl(txtOcrDataSource: s1()));
+
+  //usecase
+  s1.registerLazySingleton(() => TxtOcrUseCase(txtOcrRepository: s1()));
+
+  //image picker
+  s1.registerLazySingleton(() => ImagePicker());
+
+  // //text recognition
+  // s1.registerSingletonAsync(() => ());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,56 @@
+import 'package:camera/camera.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:ocr_text_recognition/screens/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ocr_text_recognition/features/txt_ocr/presentation/bloc/txt_ocr_cubit.dart';
+import 'package:ocr_text_recognition/features/txt_ocr/presentation/pages/text_recognition.dart';
+import 'injection_container.dart' as di;
+import 'injection_container.dart';
+
+CameraDescription? cameraDescription;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Document Scanner",
-      home: HomePage(),
-    ),
-  );
+  final camera = await availableCameras();
+  final firstCamera = camera.first;
+  cameraDescription = firstCamera;
+  await Firebase.initializeApp();
+  await di.init();
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Ocr Text Recognition",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => s1<TxtOcrCubit>(),
+          ),
+        ],
+        child: TextRecognition(
+          cameraDescription: cameraDescription!,
+        ),
+      ),
+    );
+  }
+}
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: "Document Scanner",
+//       home: HomePage(),
+//     ),
+//   );
+// }
